295c4d5bac7de75cb32e99c2d29e520c7f6ac407
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-09-11 14:00:04 +1200

    Add debug symbols

--- bootloader/bsp_dev.h
+++ bootloader/bsp_dev.h
@@ -5,4 +5,11 @@
 #ifndef BSP_DEV_H_
 #define BSP_DEV_H_
 
+// override debug macros for driver
+#include <assert.h>
+#define DRV_ASSERT(x) assert((x))
+
+// override debug macro for apps
+#define APP_ASSERT(x) assert((x))
+
 #endif //EFR32_BOOTLOADER_BSP_DEV_H_H
--- bootloader/bsp_ncp.h
+++ bootloader/bsp_ncp.h
@@ -5,6 +5,11 @@
 #ifndef BSP_NCP_H_
 #define BSP_NCP_H_
 
+// override debug macros for driver
+#include <assert.h>
+#define DRV_ASSERT(x) assert((x))
 
+// override debug macro for apps
+#define APP_ASSERT(x) assert((x))
 
 #endif //EFR32_BOOTLOADER_BSP_NCP_H
