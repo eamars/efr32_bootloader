347178c4d0e144c77f3cfeb88c277a20ceb8631d
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-12-13 11:14:21 +1300

    Move chip errata to the start of main

--- bootloader/bootloader.c
+++ bootloader/bootloader.c
@@ -46,21 +46,21 @@ static volatile uint32_t systick_counter = 0UL;
 
 void SysTick_Handler(void)
 {
-	systick_counter += 1;
+    systick_counter += 1;
 
-	if (systick_counter >= led_counter)
-	{
-		led_counter += 1000;
+    if (systick_counter >= led_counter)
+    {
+        led_counter += 1000;
 
 #if (BOARD_DEV == 1 || BOARD_HATCH_OUTDOOR_V2 == 1)
-		GPIO_PinModeSet(BTL_LED0_PORT,
-		                BTL_LED0_PIN,
-		                gpioModeInputPull,
-		                (uint32_t) green_led_state);
+        GPIO_PinModeSet(BTL_LED0_PORT,
+                        BTL_LED0_PIN,
+                        gpioModeInputPull,
+                        (uint32_t) green_led_state);
 #endif
 
-		green_led_state = !green_led_state;
-	}
+        green_led_state = !green_led_state;
+    }
 }
 
 
@@ -77,168 +77,153 @@ void store_reset_reason(void)
 
 void bootloader(void)
 {
-	// runtime patch
-	CHIP_Init();
-
     // keep the reset reason
     // store_reset_reason();
 
-	// enable clock to the GPIO to allow input to be configured
-	CMU_ClockEnable(cmuClock_GPIO, true);
+    // enable clock to the GPIO to allow input to be configured
+    CMU_ClockEnable(cmuClock_GPIO, true);
 
-	// enable systick
-	SysTick_Config(CMU_ClockFreqGet( cmuClock_CORE ) / 1000);
+    // enable systick
+    SysTick_Config(CMU_ClockFreqGet( cmuClock_CORE ) / 1000);
 
-	// initialize flash driver
-	MSC_Init();
+    // initialize flash driver
+    MSC_Init();
 
-	uart_device_init();
-	io_device * uart_device = get_uart_io_device();
+    uart_device_init();
+    io_device * uart_device = get_uart_io_device();
 
-	// create communication module
-	communication_t comm;
+    // create communication module
+    communication_t comm;
 
-	// initialize communication module (send and receive program)
-	communication_init(&comm, uart_device, comm_cb_data, comm_cb_inst);
+    // initialize communication module (send and receive program)
+    communication_init(&comm, uart_device, comm_cb_data, comm_cb_inst);
 
-	// initialize bootloader config
-	bootloader_config.base_addr = INVALID_BASE_ADDR;
-	bootloader_config.block_size_exp = 0;
-	stack32_init(&bootloader_config.base_addr_stack, 2);
+    // initialize bootloader config
+    bootloader_config.base_addr = INVALID_BASE_ADDR;
+    bootloader_config.block_size_exp = 0;
+    stack32_init(&bootloader_config.base_addr_stack, 2);
 
 
-	while (1)
-	{
-		while (communication_ready(&comm))
-		{
+    while (1)
+    {
+        while (communication_ready(&comm))
+        {
 #if (BOARD_DEV == 1 || BOARD_HATCH_OUTDOOR_V2 == 1)
-			// Blue LED
-			GPIO_PinModeSet(BTL_LED1_PORT,
-			                BTL_LED1_PIN,
-			                gpioModeInputPull,
-			                (uint32_t) blue_led_state);
-			blue_led_state = !blue_led_state;
+            // Blue LED
+            GPIO_PinModeSet(BTL_LED1_PORT,
+                            BTL_LED1_PIN,
+                            gpioModeInputPull,
+                            (uint32_t) blue_led_state);
+            blue_led_state = !blue_led_state;
 #endif
 
-			communication_receive(&comm);
-		}
-	}
+            communication_receive(&comm);
+        }
+    }
 }
 
 bool is_button_override(void)
 {
 #if (BOARD_HATCH_OUTDOOR_V2 == 1 || BOARD_DEV == 1)
-	bool pressed;
+    bool pressed;
 
-	// Enable GPIO clock
-	CMU->HFBUSCLKEN0 |= CMU_HFBUSCLKEN0_GPIO;
+    // Enable GPIO clock
+    CMU_ClockEnable(cmuClock_HFPER, true);
+    CMU_ClockEnable(cmuClock_GPIO, true);
 
-	// Since the button may have decoupling caps, they may not be charged
-	// after a power-on and could give a false positive result. To avoid
-	// this issue, drive the output as an output for a short time to charge
-	// them up as quickly as possible.
-	GPIO_PinModeSet(BTL_GPIO_ACTIVATION_PORT,
-	                BTL_GPIO_ACTIVATION_PIN,
-	                gpioModePushPull,
-	                BTL_GPIO_ACTIVATION_POLARITY);
-	for (volatile int i = 0; i < 100; i++);
+    // Reconfigure as an input with pull(up|down) to read the button state
+    GPIO_PinModeSet(BTL_GPIO_ACTIVATION_PORT,
+                    BTL_GPIO_ACTIVATION_PIN,
+                    gpioModeInputPull,
+                    BTL_GPIO_ACTIVATION_POLARITY);
 
-	// Reconfigure as an input with pull(up|down) to read the button state
-	GPIO_PinModeSet(BTL_GPIO_ACTIVATION_PORT,
-	                BTL_GPIO_ACTIVATION_PIN,
-	                gpioModeInputPull,
-	                BTL_GPIO_ACTIVATION_POLARITY);
+    pressed = GPIO_PinInGet(BTL_GPIO_ACTIVATION_PORT, BTL_GPIO_ACTIVATION_PIN)
+              != BTL_GPIO_ACTIVATION_POLARITY;
 
-	// We have to delay again here so that if the button is depressed the
-	// cap has time to discharge again after being charged up by the above delay
-	for (volatile int i = 0; i < 500; i++);
+    // Disable GPIO pin
+    GPIO_PinModeSet(BTL_GPIO_ACTIVATION_PORT,
+                    BTL_GPIO_ACTIVATION_PIN,
+                    gpioModeDisabled,
+                    BTL_GPIO_ACTIVATION_POLARITY);
 
-	pressed = GPIO_PinInGet(BTL_GPIO_ACTIVATION_PORT, BTL_GPIO_ACTIVATION_PIN)
-	          != BTL_GPIO_ACTIVATION_POLARITY;
+    // Disable GPIO clock
+    CMU_ClockEnable(cmuClock_GPIO, false);
+    CMU_ClockEnable(cmuClock_HFPER, false);
 
-	// Disable GPIO pin
-	GPIO_PinModeSet(BTL_GPIO_ACTIVATION_PORT,
-	                BTL_GPIO_ACTIVATION_PIN,
-	                gpioModeDisabled,
-	                BTL_GPIO_ACTIVATION_POLARITY);
-
-	// Disable GPIO clock
-	CMU->HFBUSCLKEN0 &= ~CMU_HFBUSCLKEN0_GPIO;
-
-	return pressed;
+    return pressed;
 #else
-	// if no button present, then we skip the button override
-	return false;
+    // if no button present, then we skip the button override
+    return false;
 #endif
 }
 
 bool is_boot_to_bootloader(void)
 {
-	uint16_t reset_reason = 0;
+    uint16_t reset_reason = 0;
 
-	// map reset cause structure to the begin of crash info memory
-	const BootloaderResetCause_t * reset_cause = (BootloaderResetCause_t *) &__RESETINFO__begin;
+    // map reset cause structure to the begin of crash info memory
+    const BootloaderResetCause_t * reset_cause = (BootloaderResetCause_t *) &__RESETINFO__begin;
 
-	// if the signature is valid, then use reset reason direction
-	if (reset_cause->signature == BOOTLOADER_RESET_SIGNATURE_VALID)
-	{
-		reset_reason = reset_cause->reason;
-	}
+    // if the signature is valid, then use reset reason direction
+    if (reset_cause->signature == BOOTLOADER_RESET_SIGNATURE_VALID)
+    {
+        reset_reason = reset_cause->reason;
+    }
 
-		// otherwise assign some reason
-	else
-	{
-		// if the signature is not set
-		if (reset_cause->signature == 0)
-		{
-			reset_reason = BOOTLOADER_RESET_REASON_BOOTLOAD;
-			if (reset_cause->reason != 1)
-			{
-				reset_reason = BOOTLOADER_RESET_REASON_UNKNOWN;
-			}
-		}
-		else
-		{
-			reset_reason = BOOTLOADER_RESET_REASON_UNKNOWN;
-		}
-	}
+        // otherwise assign some reason
+    else
+    {
+        // if the signature is not set
+        if (reset_cause->signature == 0)
+        {
+            reset_reason = BOOTLOADER_RESET_REASON_BOOTLOAD;
+            if (reset_cause->reason != 1)
+            {
+                reset_reason = BOOTLOADER_RESET_REASON_UNKNOWN;
+            }
+        }
+        else
+        {
+            reset_reason = BOOTLOADER_RESET_REASON_UNKNOWN;
+        }
+    }
 
-	// enter the bootloader only if the reboot is requested by application (not any hardware failure)
-	if (RMU->RSTCAUSE & RMU_RSTCAUSE_SYSREQRST)
-	{
-		// depending on reset reason, we will decide whether to enter bootloader
-		switch(reset_reason)
-		{
-			case BOOTLOADER_RESET_REASON_BOOTLOAD:
-			case BOOTLOADER_RESET_REASON_FORCE:
-			case BOOTLOADER_RESET_REASON_UPGRADE:
-			case BOOTLOADER_RESET_REASON_BADAPP:
-				return true;
+    // enter the bootloader only if the reboot is requested by application (not any hardware failure)
+    if (RMU->RSTCAUSE & RMU_RSTCAUSE_SYSREQRST)
+    {
+        // depending on reset reason, we will decide whether to enter bootloader
+        switch(reset_reason)
+        {
+            case BOOTLOADER_RESET_REASON_BOOTLOAD:
+            case BOOTLOADER_RESET_REASON_FORCE:
+            case BOOTLOADER_RESET_REASON_UPGRADE:
+            case BOOTLOADER_RESET_REASON_BADAPP:
+                return true;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 bool is_boot_to_app(uint32_t * app_addr)
 {
-	// map reset cause structure to the begin of crash info memory
-	const ExtendedBootloaderResetCause_t * reset_cause = (ExtendedBootloaderResetCause_t *) &__RESETINFO__begin;
+    // map reset cause structure to the begin of crash info memory
+    const ExtendedBootloaderResetCause_t * reset_cause = (ExtendedBootloaderResetCause_t *) &__RESETINFO__begin;
 
-	if (reset_cause->basicResetCause.signature == BOOTLOADER_RESET_SIGNATURE_VALID &&
-		reset_cause->basicResetCause.reason == BOOTLOADER_RESET_REASON_GO &&
-		reset_cause->app_signature == APP_SIGNATURE &&
-		reset_cause->app_addr != INVALID_BASE_ADDR)
-	{
-		*app_addr = reset_cause->app_addr;
+    if (reset_cause->basicResetCause.signature == BOOTLOADER_RESET_SIGNATURE_VALID &&
+        reset_cause->basicResetCause.reason == BOOTLOADER_RESET_REASON_GO &&
+        reset_cause->app_signature == APP_SIGNATURE &&
+        reset_cause->app_addr != INVALID_BASE_ADDR)
+    {
+        *app_addr = reset_cause->app_addr;
 
-		return true;
-	}
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 
@@ -251,9 +236,9 @@ bool is_boot_to_prev_app(uint32_t * app_addr)
 
 void clear_boot_request(void)
 {
-	// map reset cause structure to the begin of crash info memory
-	ExtendedBootloaderResetCause_t * reset_cause = (ExtendedBootloaderResetCause_t *) &__RESETINFO__begin;
+    // map reset cause structure to the begin of crash info memory
+    ExtendedBootloaderResetCause_t * reset_cause = (ExtendedBootloaderResetCause_t *) &__RESETINFO__begin;
 
-	reset_cause->app_signature = 0UL;
-	reset_cause->app_addr = INVALID_BASE_ADDR;
+    reset_cause->app_signature = 0UL;
+    reset_cause->app_addr = INVALID_BASE_ADDR;
 }
--- bootloader/main.c
+++ bootloader/main.c
@@ -4,50 +4,69 @@
  */
 #include <stdint.h>
 
+#include "em_chip.h"
+
+#include "irq.h"
 #include "bootloader.h"
 #include "bootloader_api.h"
 
 int main(void)
 {
-	uint32_t app_addr;
+    uint32_t app_addr;
 
-	// check if button is pressed
-	if (is_button_override())
-	{
-		bootloader();
-	}
+    // runtime patch
+    CHIP_Init();
 
-	// check if other request to boot into bootloader
-	if (is_boot_to_bootloader())
-	{
-		bootloader();
-	}
+    // check if button is pressed
+    if (is_button_override())
+    {
+        bootloader();
+    }
 
-	// if there is no request to boot to bootloader, we then check if there is special request for booting into
-	// specific application
-	app_addr = INVALID_BASE_ADDR;
-	if (is_boot_to_app(&app_addr))
-	{
-		clear_boot_request();
-		branch_to_addr(app_addr);
-	}
+    // check if other request to boot into bootloader
+    if (is_boot_to_bootloader())
+    {
+        bootloader();
+    }
 
-	// depending on the last boot partition, unless there is special request that requires to boot to other partition,
-	// (has handled in previous case, the bootloader should boot the application to its last boot address, or default
-	// address.
-	app_addr = INVALID_BASE_ADDR;
-	if (is_boot_to_prev_app(&app_addr))
-	{
-		branch_to_addr(app_addr);
-	}
+    // if there is no request to boot to bootloader, we then check if there is special request for booting into
+    // specific application
+    app_addr = INVALID_BASE_ADDR;
+    if (is_boot_to_app(&app_addr))
+    {
+        clear_boot_request();
+        branch_to_addr(app_addr);
+    }
 
-	// otherwise boot to bootloader
-	bootloader();
+    // depending on the last boot partition, unless there is special request that requires to boot to other partition,
+    // (has handled in previous case, the bootloader should boot the application to its last boot address, or default
+    // address.
+    app_addr = INVALID_BASE_ADDR;
+    if (is_boot_to_prev_app(&app_addr))
+    {
+        branch_to_addr(app_addr);
+    }
 
-	// trap here
+    // otherwise boot to bootloader
+    bootloader();
 
-	while (1)
-	{
+    // trap here
 
-	}
+    while (1)
+    {
+
+    }
+}
+
+
+__attribute__ ((optimize("-O0")))
+void HardFault_Handler(void)
+{
+    HFSR_t hfsr;
+    hfsr.HFSR = SCB->HFSR;
+
+    while (1)
+    {
+
+    }
 }
